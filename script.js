6ef49f3e032c0718fb98acea9f5cc58221b3a8e8
Spencer Scarpelli <spencerscarpelli@gmail.com>
2017-01-09 18:25:40 -0800

    finish adding jquery load statements

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 (function() {
   'use strict';
 
-  var projectHeights = {
-    'twitter-sa': '1500px'
-  }
+  $('#project-twitter-sa').load('./project-views/twitter-sentiment-analysis');
+  $('#project-in-other-words').load('./project-views/in-other-words');
+  $('#project-angular-shopping-cart').load('./project-views/angular-shopping-cart');
+  $('#project-sommbuddy').load('./project-views/sommbuddy');
+  $('#project-8k-crawler').load('./project-views/8k-crawler');
+  $('#project-scarp').load('./project-views/scarp');
 
   $('.project').on('click', e => {
     let targetId = e.target.parentElement.id;
